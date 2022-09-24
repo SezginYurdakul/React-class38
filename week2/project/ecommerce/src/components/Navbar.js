56d9770bf53a167df1b7fcc9ac0335681b1828d4
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-09-24 15:45:04 +0200

    Initial commit for week2

--- week2/project/ecommerce/src/components/Navbar.js
+++ week2/project/ecommerce/src/components/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 const Navbar = ({ selectCategory, selectedCategory, categoryList }) => {
-  //let newCategoryList = categoryList.map(category => category.slice(6).toUpperCase());
   return (
-    <div className="Navbar">
+    <div className="navbar">
       <ul>
         {categoryList.map(category =>
-          <li key={category} className={selectedCategory === category ? "selectedCategory" : null}
+          <li key={category} className={selectedCategory === category && 'selectedCategory'}
             onClick={() => selectCategory(category)}>
             {category.slice(6).toUpperCase()}
           </li>)}
